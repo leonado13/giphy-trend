a21be29287927dadfd8c481c273305982e9745d4
Tuan Nguyen <ng.nhat.tuan@gmail.com>
2019-07-25 00:36:14 +0700

    remove trunk and add structure

---
+++ src/components/TrendItem/TrendItem.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class TrendItem extends Component{
+  render() {
+    return (
+      <div>1</div>
+    );
+  }
+}
+
+export default TrendItem;
---
+++ src/components/TrendList/TrendList.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+import TrendItem from "components/TrendItem/TrendItem";
+
+class TrendList extends Component {
+  render() {
+    return (
+      <div>
+        <TrendItem/>
+      </div>
+    );
+  }
+}
+
+export default TrendList;
